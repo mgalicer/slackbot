cf8f6d43770160c129663664d4c7c819355a7128
Mari <marigalicer@gmail.com>
2016-04-25 18:56:16 -0400

    add persistent data

--- app.js
+++ app.js
@@ -7,7 +7,10 @@ var env = require('dotenv');
 require('dotenv').config();
 
 var Firebase = require("firebase");
-var myFirebaseRef = new Firebase("https://slackbot1.firebaseio.com/");
+var ref = new Firebase("https://slackbot1.firebaseio.com/");
+var usersRef = ref.child("users");
+
+var fridaySayings = ["happ"]
 
 var bot = controller.spawn({
     token: process.env.SLACK_TOKEN
@@ -22,19 +25,35 @@ bot.startRTM(function(err, bot, payload) {
 var questions = ["question 1?", "question 2?", "question 3?"];
 var responses = {}
 
-function askQuestions(convo){
-    questions.forEach(function(question){
-        convo.ask(question, function(response, convo){
-            responses[response.question] = response.text;
-            convo.next();
-        })
-    })
-}
-
 controller.hears([/./], ['direct_message'], function(bot, message){
     bot.startConversation(message, function(err, convo){
         convo.say("Hey there! Let's get this party started.")
-        askQuestions(convo);
+        askQuestions(convo, message);
     })
 })
 
+function askQuestions(convo, message){
+    questions.forEach(function(question){
+        convo.ask(question, function(response, convo){
+            var timestamp = new Date();
+            responses[response.question] = response.text;
+            saveToFirebase(message.user, timestamp, responses)
+            convo.next();
+        })
+    })
+    if(new Date().getDay === 5){
+        analyzeWeek(convo)
+    }
+}
+
+function saveToFirebase(user, timestamp, responses){
+    usersRef.set({
+      user: {
+        timestamp: responses
+      }
+    });
+}
+
+function analyzeWeek(){
+    convo.say()
+}
